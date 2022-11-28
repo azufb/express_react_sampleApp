11f71abefbd27264c2eaa0876874fc991fa5f444
azufb <curiousazunas@gmail.com>
2022-11-28 14:31:19 +0900

    サンプルフォーム作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,43 @@
 import { axiosClient } from './axios';
+import { useForm } from 'react-hook-form';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+  const { register, handleSubmit } = useForm();
+  const [state, setState] = useState<any>();
 
   const handleGETApiResponse = async () => {
     const response = await axiosClient.get('/api');
     console.log(response.data.message);
-  }
+  };
+
+  const onSubmit = async (data: any) => {
+    const param = {
+      title: data.title
+    };
+
+    const response = await axiosClient.post('/addTask', param);
+    setState(response);
+  };
+
+  useEffect(() => {
+    const func = async () => {
+      const getResponse = await axiosClient.get('/getTasks');
+      console.log(getResponse);
+    };
+
+    func();
+  }, [state])
 
   return (
     <div>
       <button onClick={handleGETApiResponse}>クリック！</button>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+          <label>タスク：</label>
+          <input {...register('title')} />
+          <input type='submit' />
+      </form>
     </div>
   )
 }
